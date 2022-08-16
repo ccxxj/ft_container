4acbb14e1be89ebf758679106eadfb4bdc3697ac
Xiaojing <cocoxxj@gmail.com>
2022-08-16 19:39:32 +0200

    add iterator

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include "vector.hpp"
+#include <iostream>
 
 int main() {
 	//ft::vector<int> a((unsigned int)3, (const long)5);
-	ft::vector<int> a;
-	ft::vector<int> b(1, 2);
+	try{
+		ft::vector<int> a;
+		ft::vector<int> b(1, 2);
+		std::cout << b[0] << std::endl;
+		b[0] = 5;
+		std::cout << b[0] << std::endl;
+		b.assign(5, 3);
+		std::cout << "within bound " << b[0] << std::endl;
+		std::cout << "out of bound " << b[7] << std::endl;
+		std::cout << "within bound " << b.at(4) << std::endl;
+		std::cout << "out of bound " << b.at(7) << std::endl;
+		std::cout << "within bound for front " << b.front() << std::endl;
+		std::cout << "within bound for back " << b.back() << std::endl;
+		std::cout << "out of bound for front " << a.front() << std::endl;
+		std::cout << "out of bound for back " << a.back() << std::endl;
+	}
+	catch(std::exception) {
+		std::cout << "caught exception\n";
+	}
+
+
+
+	
 }
--- vector.hpp
+++ vector.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <iostream>
 
 typedef size_t size_type;	
 
@@ -9,28 +10,78 @@ namespace ft {
 	template<typename T>
 	class vector{
 		private:
-			T *data;
+			T *dataArray;
+			std::allocator<T> alloc;
+			size_type dataSize;//TODO check if this is needed. Also make sure to update properly, e.g. push back. Should this be the actual size, or the capacity??
+
+			struct Iterator {//implement iterator per container
+				typedef typename T value_type;
+				typedef typename ptrdiff_t difference_type;
+				typedef typename random_access_iterator_tag iterator_category;//TODO is this the right tag to go>>
+				typedef typename T* pointer;
+				typedef typename T& reference;
+
+				Iterator(pointer inputPtr): ptr(inputPtr) {}
+
+				private:
+					pointer ptr;
+			}
 		
 		public:
-		//there is more versions per function
-			//constructor
-			vector() {}
+		/*******Member function*******/	
+			/******constructor******/
+			vector() {dataSize = 0;}
 			// vector() noexcept(noexcept(Allocator())); TODO this is for c++ 17
-			
 			explicit vector(size_type count, const T& value){
-				std::allocator<T> a;
-				data = a.allocate(count);
+				dataSize = count;
+				dataArray = alloc.allocate(count);
 				for(size_type i = 0; i < count; i++) {
-					data[i] = value;
+					dataArray[i] = value;
 				}
 			}
-			
 			// explicit vector(size_type count, const T& value, const Allocator& alloc = Allocator());
+			/******Others******/
+			~vector() {
+				alloc.deallocate(dataArray, dataSize);
+			}
+			vector& operator=(const vector& other) {
 
-			// ~vector();
-			// vector& operator=(const vector& other);
-			// void assign(size_type count, const T& value);
-			// allocator_type get_allocator() const;
+			}
+			void assign(size_type count, const T& value) {
+				alloc.deallocate(dataArray, dataSize);
+				dataArray = alloc.allocate(count);
+				dataSize = count;
+				for(size_type i = 0; i < count; i++) {
+					dataArray[i] = value;
+				}
+			}
+			std::allocator<T> get_allocator() const {
+				return alloc;
+			}
+
+			/******Element access******/
+			T& at(size_type pos) {
+				if(pos >= dataSize)
+					throw std::out_of_range("out of range\n");
+				else
+					return dataArray[pos];
+			}
+
+			T& operator[](size_type pos) {//TODO is this indeed return as reference instead of copy of value
+				return dataArray[pos];
+			}
+
+			T& front() {
+				return dataArray[0];
+			}
+
+			T& back() {
+				return dataArray[dataSize - 1];
+			}
+
+			T *data() {
+				return dataArray;
+			}
 
 		// friend:
 
